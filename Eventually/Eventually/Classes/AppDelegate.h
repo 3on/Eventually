4fedf743046bc38e4fda7ba0a89deac03c7c1f75
3on <jr@3on.fr>
2011-12-13 18:36:48 +0100

    PhoneGap's shell

---
+++ Eventually/Eventually/Classes/AppDelegate.h
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.h
+//  Eventually
+//
+//  Created by Jean Romain Prevost on 13/12/11.
+//  Copyright 3on 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#ifdef PHONEGAP_FRAMEWORK
+	#import <PhoneGap/PhoneGapDelegate.h>
+#else
+	#import "PhoneGapDelegate.h"
+#endif
+
+@interface AppDelegate : PhoneGapDelegate {
+
+	NSString* invokeString;
+}
+
+// invoke string is passed to your app on launch, this is only valid if you 
+// edit Eventually.plist to add a protocol
+// a simple tutorial can be found here : 
+// http://iphonedevelopertips.com/cocoa/launching-your-own-application-via-a-custom-url-scheme.html
+
+@property (copy)  NSString* invokeString;
+
+@end
+
---
+++ Eventually/Eventually/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Eventually
+//
+//  Created by Jean Romain Prevost on 13/12/11.
+//  Copyright 3on 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
